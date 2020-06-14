c15c8f1d792cd023c5f4305872e62235796299de
Linux1230 <pekker77@gmail.com>
2020-06-14 16:00:06 +0200

    start

---
+++ CSharpExtensions/CSharpExtensions/String/ContainsCaseInsensitive.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharpExtensions.String
+{
+    //https://codeexamples.dev/csharp-string-contains-insensitive/
+    public static class Contains
+    {
+        public static bool ContainsCaseInsensitive(this string text, string value,
+            StringComparison stringComparison = StringComparison.CurrentCultureIgnoreCase)
+        {
+            return text.IndexOf(value, stringComparison) >= 0;
+        }
+    }
+}
